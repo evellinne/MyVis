ffc69d6d1e97a261a05334dbc6aba7dda502f6e2
Luiz Braga <luizbragaa@gmail.com>
2012-06-19 00:58:31 -0300

    v1.1

--- src/ufrpe/myvis/data/Usuario.java
+++ src/ufrpe/myvis/data/Usuario.java
@@ -1,5 +1,17 @@
 package ufrpe.myvis.data;
 
 public class Usuario {
+	private String id;
+	
+	public Usuario (String id){
+		this.setID(id);		
+	}
+	
+	public String getID() {
+		return id;
+	}
+	public void setID(String id) {
+		this.id = id;
+	}
 
 }
